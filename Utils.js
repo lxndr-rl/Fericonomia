3b2cceb3e908597ad275feee6b6fc10fde478e4f
71walceli <antonini2903@hotmail.com>
2022-06-29 23:22:00 -0500

    Funciones para los cálculos documentadas

--- Utils.js
+++ Utils.js
@@ -1,8 +1,22 @@
+import { ProgressViewIOSBase } from "react-native"
 import { Datos } from "./DataContext"
 
 export const dolares = (cantidad, redondearDigitos=2) => 
     `$ ${ cantidad.toFixed(redondearDigitos) }`
 
+
+/**
+ * @param {{
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ * }} recurso
+ * @returns Number
+ */
 export const calcularSubtotalRecurso = (recurso) => {
     //const costoServicioBasico = calcularCostoServicioBásico(recurso)
 
@@ -25,6 +39,203 @@ export const calcularSubtotalRecurso = (recurso) => {
     // TODO Agregar otros valores que puedan totalizar
     }
 
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalMateriasDirectas = (recursos) => recursos
+    .filter(recurso => recurso.tipo === "materia" && recurso.esCostoDirecto)
+    .reduce(
+        (anterior, actual) => anterior + calcularSubtotalRecurso(actual)
+        , 0)
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalMateriasIndirectas = (recursos) => recursos
+    .filter(recurso => recurso.tipo === "materia" && !recurso.esCostoDirecto)
+    .reduce(
+        (anterior, actual) => anterior + calcularSubtotalRecurso(actual)
+        , 0)
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalManoObraDirecta = (recursos) => recursos
+    .filter(recurso => recurso.tipo === "manoObra" && recurso.esCostoDirecto)
+    .reduce(
+        (anterior, actual) => anterior + calcularSubtotalRecurso(actual)
+        , 0)
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalManoObraIndirecta = (recursos) => recursos
+    .filter(recurso => recurso.tipo === "manoObra" && !recurso.esCostoDirecto)
+    .reduce(
+        (anterior, actual) => anterior + calcularSubtotalRecurso(actual)
+        , 0)
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalOtrosIndirectos = (recursos) => recursos
+    .filter(recurso => recurso.tipo === "otro" && !recurso.esCostoDirecto)
+    .reduce(
+        (anterior, actual) => anterior + calcularSubtotalRecurso(actual)
+        , 0)
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalDirectoUnitario = (recursos) => 
+    calcularSubtotalMateriasDirectas(recursos) + calcularSubtotalManoObraDirecta(recursos)
+
+/**
+ * @param {{producto: {
+ *  cantidad: number
+ *  recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  }} producto
+ * @returns Number
+ */
+export const calcularSubtotalDirectoTotal = (producto) => 
+    calcularSubtotalDirectoUnitario(producto.recursos) * producto.cantidad
+
+/**
+ * @param {{recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  ]
+ * }} recursos
+ * @returns Number
+ */
+export const calcularSubtotalIndirectoTotal = (recursos) => 
+    calcularSubtotalMateriasIndirectas(recursos) + calcularSubtotalManoObraIndirecta(recursos)
+        + calcularSubtotalOtrosIndirectos(recursos)
+
+/**
+ * @param {{producto: {
+ *  cantidad: number
+ *  recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  }} producto
+ * @returns Number
+ */
+export const calcularCostoTotal = (producto) => 
+    calcularSubtotalDirectoTotal(producto) + calcularSubtotalIndirectoTotal(producto.recursos)
+
+/**
+ * @param {{producto: {
+ *  cantidad: number
+ *  recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  }} producto
+ * @returns Number
+ */
+export const calcularCostoTotalUnitario = (producto) => 
+    calcularCostoTotal(producto) / producto.cantidad
+
+/**
+ * @param {{producto: {
+ *  cantidad: number
+ *  recursos: [
+ *      tipo: String,
+ *      esCostoDirecto: Boolean,
+ *      unidad: String,
+ *      cantidad: Number,
+ *      unidadCosto: Number,
+ *      personasCantidad: Number,
+ *      costo: Number,
+ *  }} producto
+ * @returns Number
+ */
+export const calcularPrecioVenta = (producto) =>
+    calcularCostoTotalUnitario(producto) *(1 + producto.margenUtilidad)
+
 // TODO Subtotales de costos segú sea. Uno para cada tipo
 // - Directo: Mano obra, materias
 // - Indirecto: Mano obra, materias, otros
+
